2797ecb027df9ded557a60d8b66588ef330da790
Isaias233-ops <macarao233@gmail.com>
2025-09-24 21:32:37 -0300

    Atualização do CSS, do HTML
    Adicionei as cores, margem, espaçamento, formatação da letra no CSS.
    Adicionei a tag section ao HTML

--- scrips/ra-rv.js
+++ scrips/ra-rv.js
@@ -1,14 +1,14 @@
-const title = "Realidade Virtual e Realidade Aumentada"
-const titleElement = document.getElementById("title")
-let index = 0
-console.log(title.length)
+const title = "Realidade Virtual e Realidade Aumentada";
+const titleElement = document.getElementById("title");
+let index = 0;
+console.log(title.length);
 
 function typeWriter() {
-    if(index < title.length) {
-        titleElement.textContent += title.charAt(index)
-        index++
-        setTimeout(typeWriter, 200)
-    }
+  if (index < title.length) {
+    titleElement.textContent += title.charAt(index);
+    index++;
+    setTimeout(typeWriter, 200);
+  }
 }
 
-window.onload = typeWriter
+window.onload = typeWriter;
